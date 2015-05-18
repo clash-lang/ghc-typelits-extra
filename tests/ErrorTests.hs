d1f118e3dfa2c68249c1f05bd08a560c9784ba40
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-05-18 18:06:38 +0200

    Add tests that combines solvers
    In this case, the combination of ghc-typelits-extra and
    ghc-typelits-natnormalise

---
+++ tests/ErrorTests.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DataKinds, TypeOperators #-}
+
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Extra.Solver #-}
+
+module ErrorTests where
+
+import GHC.TypeLits
+import GHC.TypeLits.Extra
+import Data.Proxy
+
+testFail1 :: Proxy (GCD 6 8) -> Proxy 3
+testFail1 = id
+
+testFail2 :: Proxy ((GCD 6 8) + x) -> Proxy (x + 3)
+testFail2 = id
+
+testFail1Errors =
+  ["Expected type: Proxy (GCD 6 8) -> Proxy 3"
+  ,"Actual type: Proxy 3 -> Proxy 3"
+  ]
+
+testFail2Errors =
+  ["Expected type: Proxy (GCD 6 8 + x) -> Proxy (x + 3)"
+  ,"Actual type: Proxy (x + 3) -> Proxy (x + 3)"
+  ]
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,9 +1,48 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, TypeOperators, ScopedTypeVariables #-}
 
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Extra.Solver #-}
 
+import Data.List (isInfixOf)
+import Control.Exception
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import ErrorTests
+
+import GHC.TypeLits
 import GHC.TypeLits.Extra
 import Data.Proxy
 
 test1 :: Proxy (GCD 6 8) -> Proxy 2
 test1 = id
+
+test2 :: Proxy ((GCD 6 8) + x) -> Proxy (x + 2)
+test2 = id
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "ghc-typelits-natnormalise"
+  [ testGroup "Basic functionality"
+    [ testCase "Proxy (GCD 6 8) -> Proxy 2" $
+      show (test1 Proxy) @?=
+      "Proxy"
+    , testCase "Proxy ((GCD 6 8) + x) -> Proxy (x + 2)" $
+      show (test2 Proxy) @?=
+      "Proxy"
+    ]
+  , testGroup "errors"
+    [ testCase "Proxy (GCD 6 8) -> Proxy 3" $ testFail1 `throws` testFail1Errors
+    , testCase "Proxy ((GCD 6 8) + x) -> Proxy (x + 3)" $ testFail2 `throws` testFail2Errors
+    ]
+  ]
+
+-- | Assert that evaluation of the first argument (to WHNF) will throw
+-- an exception whose string representation contains the given
+-- substrings.
+throws :: a -> [String] -> Assertion
+throws v xs =
+  (evaluate v >> assertFailure "No exception!")
+  `catch` \ (e :: SomeException) -> if all (`isInfixOf` show e) xs then return () else throw e
